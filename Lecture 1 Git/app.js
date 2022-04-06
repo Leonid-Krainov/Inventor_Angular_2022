a9e7c553e536869fd3301f49ca65abe02cb28aef
Leonid Krainov <krajnovleonid@gmail.com>
2022-04-06 18:21:04 +0300

    Add task 2

--- Lecture 1 Git/app.js
+++ Lecture 1 Git/app.js
@@ -19,3 +19,18 @@ function calculate(str) {
     }
     return ".".repeat(dotAmount);
 }
+
+// task 2
+
+function grid(n) {
+    if (n===0) return null;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    for (let i=0 ; i<n ; i++) {
+        if (i+n > alphabet.length) {
+            console.log (alphabet.substring(i) + alphabet.substring(0,i+n-26));
+        }
+        else {
+            console.log(alphabet.substring(i, i + n));
+        }
+    }
+}
